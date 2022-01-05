8512a81f8b91f0e924e4be8bdbddbb4265d6b2f7
Arul Valan Anto S <arulvalananto@gmail.com>
2022-01-05 15:57:12 +0530

    node updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL:
+    "https://infinite-sands-32683.herokuapp.com/api/v1/" ||
+    process.env.REACT_APP_API_URL,
   withCredentials: true,
   timeout: 10000,
   timeoutErrorMessage: "Server Down",
