1e61da599516eae82465215ac4227f5ef7d27293
Arul Valan Anto S <arulvalananto@gmail.com>
2022-01-16 08:49:21 +0530

    Implemented PWA in react app

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,42 +1,29 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: "#4b24bf",
-      secondary: "#f41368",
-      tertiary: "#2ccae6",
-      danger: "crimson",
-      moderate: "goldenrod",
-      darkgray: "#6B7280",
-      facebook: "#3C5A9A",
-      mildgray: "#1d1c1c",
-      bglight: "#1F1F23",
-      bgdark: "#0E0E10",
-      "custom-cyan": "#07AABD",
-      gray: colors.coolGray,
-      red: colors.red,
-      yellow: colors.amber,
-      green: colors.emerald,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      purple: colors.violet,
-      white: colors.white,
-      black: colors.black,
-    },
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
       rampart: ["Rampart One", "cursive"],
       jura: ["Jura", "sans-serif"],
     },
     extend: {
+      colors: {
+        primary: "#4b24bf",
+        secondary: "#f41368",
+        tertiary: "#2ccae6",
+        danger: "crimson",
+        moderate: "goldenrod",
+        darkgray: "#6B7280",
+        facebook: "#3C5A9A",
+        mildgray: "#1d1c1c",
+        bglight: "#1F1F23",
+        bgdark: "#0E0E10",
+        "custom-cyan": "#07AABD",
+      },
       backgroundImage: {
-        "signin-cover": "url('./assets/signin_cover.jpg')",
+        "signin-cover": "url('./assets/signin_cover2.jpg')",
         "forgotPassword-cover": "url('./assets/forgotPassword_cover.jpg')",
-        "credit-card-1": "url('./assets/creditcard-1.jpg')",
-        "credit-card-2": "url('./assets/creditcard-2.jpg')",
       },
       animation: {
         shake: "shake 1s ease-in-out",
